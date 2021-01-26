4f94b81a684d873babd9faa548b9df36f857f5ce
Özhan AKÇAY <ozhanakcay@aof.anadolu.edu.tr>
2021-01-26 15:42:07 +0300

    Proje dosyası ekle.

---
+++ MyDictonary/MyDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyDictionary<S>
+    {
+        S[] items;
+        
+        public MyDictionary()
+        {
+            items = new S[0];
+        }
+           
+        public void Add(S item)
+        {
+            S[] tempArray = items;
+            items = new S[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+            Console.WriteLine(item);
+        }
+
+    }
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<String> dictonary = new MyDictionary<string>();
+            dictonary.Add("Kelime");
+
+            MyDictionary<string> dictionary = new MyDictionary<string>();
+            
+            Console.WriteLine(dictionary);
+
+        }
+    }
+}
